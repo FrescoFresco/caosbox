505da1aed64a19e0183c47e3798b7bfe5da0f622
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-08 02:41:57 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-import 'package:caosbox/src/models/models.dart'   as models;
-import 'package:caosbox/src/widgets/quick_add.dart';
-import 'package:caosbox/src/widgets/chips_panel.dart';
-import 'package:caosbox/src/widgets/item_card.dart';
+import 'package:caosbox/src/models/models.dart' as models;
 import 'package:caosbox/src/utils/filter_engine.dart' as utils;
+import 'package:caosbox/src/widgets/quick_add.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CaosBox',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
+      theme: ThemeData(useMaterial3: true),
       home: const CaosBox(),
     );
   }
@@ -33,9 +25,8 @@ class CaosBox extends StatefulWidget {
 }
 
 class _CaosBoxState extends State<CaosBox> {
-  final models.AppState _state   = models.AppState();
-  final utils.FilterSet _filter  = utils.FilterSet();
-  final Set<String>     _expand  = <String>{};
+  final models.AppState _state    = models.AppState();
+  final utils.FilterSet _filter   = utils.FilterSet();
 
   @override
   void dispose() {
@@ -43,11 +34,9 @@ class _CaosBoxState extends State<CaosBox> {
     super.dispose();
   }
 
-  void _refresh() => setState(() {});
-
   @override
   Widget build(BuildContext context) {
-    final items = utils.FilterEngine.apply(
+    final ideas = utils.FilterEngine.apply(
       _state.items(models.ItemType.idea),
       _state,
       _filter,
@@ -56,32 +45,29 @@ class _CaosBoxState extends State<CaosBox> {
     return Scaffold(
       appBar: AppBar(title: const Text('CaosBox')),
       body: Padding(
-        padding: const EdgeInsets.all(12),
+        padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            QuickAdd(
-              type   : models.ItemType.idea,
-              st     : _state,
-              onAdded: _refresh,
+            QuickAdd(type: models.ItemType.idea, st: _state),
+            const SizedBox(height: 12),
+            TextField(
+              controller: _filter.text,
+              decoration: const InputDecoration(
+                hintText: 'Buscar…',
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (_) => setState(() {}),
             ),
             const SizedBox(height: 12),
-            ChipsPanel(set: _filter, onUpdate: _refresh),
-            const SizedBox(height: 12),
             Expanded(
-              child: ListView(
-                children: items.map((it) {
-                  final open = _expand.contains(it.id);
-                  return ItemCard(
-                    it         : it,
-                    st         : _state,
-                    isExpanded : open,
-                    onTapBody  : () {
-                      setState(() => open
-                          ? _expand.remove(it.id)
-                          : _expand.add(it.id));
-                    },
-                  );
-                }).toList(),
+              child: ListView.builder(
+                itemCount: ideas.length,
+                itemBuilder: (_, i) => Card(
+                  child: ListTile(
+                    title: Text(ideas[i].text),
+                    subtitle: Text(ideas[i].id),
+                  ),
+                ),
               ),
             ),
           ],
