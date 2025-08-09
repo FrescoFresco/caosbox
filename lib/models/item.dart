941c78771e8ff615ed33b34d713cbc96ff493959
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-09 13:10:37 +0100

    Update item.dart

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,22 +1,34 @@
 import 'enums.dart';
 
 class Item {
-  final String id, text;
+  final String id;
+  final String text;
   final ItemType type;
   final ItemStatus status;
-  final DateTime createdAt, modifiedAt;
+  final DateTime createdAt;
+  final DateTime modifiedAt;
   final int statusChanges;
 
-  Item(this.id, this.text, this.type,
-      [this.status = ItemStatus.normal,
-       DateTime? c, DateTime? m, this.statusChanges = 0])
-      : createdAt  = c ?? DateTime.now(),
+  Item(
+    this.id,
+    this.text,
+    this.type, [
+    this.status = ItemStatus.normal,
+    DateTime? c,
+    DateTime? m,
+    this.statusChanges = 0,
+  ])  : createdAt = c ?? DateTime.now(),
         modifiedAt = m ?? DateTime.now();
 
-  Item copyWith({ItemStatus? status}) {
-    final ns   = status ?? this.status;
-    final diff = ns != this.status;
-    return Item(id, text, type, ns, createdAt,
-        diff ? DateTime.now() : modifiedAt, diff ? statusChanges + 1 : statusChanges);
+  Item copyWith({String? text, ItemStatus? status, DateTime? modifiedAt, int? statusChanges}) {
+    return Item(
+      id,
+      text ?? this.text,
+      type,
+      status ?? this.status,
+      createdAt,
+      modifiedAt ?? this.modifiedAt,
+      statusChanges ?? this.statusChanges,
+    );
   }
 }
