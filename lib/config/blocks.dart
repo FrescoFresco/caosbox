d0096f2b79c702841e9c15359497cea1e924ce15
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-11 21:25:18 +0100

    Update blocks.dart

--- lib/config/blocks.dart
+++ lib/config/blocks.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:caosbox/core/models/enums.dart';
 import 'package:caosbox/app/state/app_state.dart';
 import 'package:caosbox/ui/screens/generic_screen.dart';
+import 'package:caosbox/ui/screens/links_block.dart';
 
 typedef ScreenBuilder = Widget Function(BuildContext, AppState);
 
@@ -12,14 +13,23 @@ class Block {
   final ItemType? type;
   final ScreenBuilder? custom;
 
-  const Block.item({required this.id, required this.icon, required this.label, required this.type})
-      : custom = null;
+  const Block.item({
+    required this.id,
+    required this.icon,
+    required this.label,
+    required this.type,
+  }) : custom = null;
 
-  const Block.custom({required this.id, required this.icon, required this.label, required this.custom})
-      : type = null;
+  const Block.custom({
+    required this.id,
+    required this.icon,
+    required this.label,
+    required this.custom,
+  }) : type = null;
 }
 
 final blocks = <Block>[
-  const Block.item(id: 'ideas',   icon: Icons.lightbulb, label: 'Ideas',   type: ItemType.idea),
+  const Block.item(id: 'ideas',   icon: Icons.lightbulb, label: 'Ideas',    type: ItemType.idea),
   const Block.item(id: 'actions', icon: Icons.assignment, label: 'Acciones', type: ItemType.action),
+  const Block.custom(id: 'links', icon: Icons.link, label: 'Enlaces',       custom: (ctx, st) => LinksBlock(state: st)),
 ];
