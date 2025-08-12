0cb937930817320c5d28b9f1366e96cec442ca3b
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-12 20:15:16 +0100

    Update generic_screen.dart

--- lib/ui/screens/generic_screen.dart
+++ lib/ui/screens/generic_screen.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:caosbox/config/blocks.dart';
 import 'package:caosbox/app/state/app_state.dart';
+import 'package:caosbox/config/blocks.dart';
 import 'package:caosbox/core/models/enums.dart';
-
 import 'package:caosbox/domain/search/search_models.dart';
 import 'package:caosbox/ui/widgets/content_block.dart';
 
-class GenericScreen extends StatelessWidget {
-  final Block block;
-  final AppState state;
-  final SearchSpec spec;
-  final String quickQuery;
-  final ValueChanged<String> onQuickQuery;
-  final Future<void> Function(BuildContext, ItemType) onOpenFilters;
+class GenericScreen extends StatefulWidget{
+  final Block b; final AppState st;
+  const GenericScreen({super.key, required this.b, required this.st});
+  @override State<GenericScreen> createState()=>_GenericScreenState();
+}
 
-  const GenericScreen({
-    super.key,
-    required this.block,
-    required this.state,
-    required this.spec,
-    required this.quickQuery,
-    required this.onQuickQuery,
-    required this.onOpenFilters,
-  });
+class _GenericScreenState extends State<GenericScreen> with AutomaticKeepAliveClientMixin{
+  String _quick = '';
+  SearchSpec _spec = const SearchSpec();
+  @override bool get wantKeepAlive => true;
 
   @override
-  Widget build(BuildContext context) {
-    final t = block.type!;
-    return ContentBlock(
-      state: state,
-      types: { t },
-      spec: spec,
-      quickQuery: quickQuery,
-      onQuickQuery: onQuickQuery,
-      onOpenFilters: () => onOpenFilters(context, t),
-      showComposer: true,
-      mode: ContentBlockMode.list,
-      checkboxSide: CheckboxSide.none,
+  Widget build(BuildContext ctx){
+    super.build(ctx);
+    final t = widget.b.type!;
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: ContentBlock(
+        state: widget.st,
+        types: {t},
+        spec: _spec,
+        quickQuery: _quick,
+        onQuickQuery: (q)=> setState(()=> _quick = q),
+        onSpecChanged: (s)=> setState(()=> _spec = s),
+        showComposer: true,
+        mode: ContentBlockMode.list,
+        checkboxSide: CheckboxSide.none,
+      ),
     );
   }
 }
