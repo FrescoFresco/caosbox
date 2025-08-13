8bb7c3604bc42b1f06782e8dd2431d35118f0aeb
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-13 10:22:58 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:caosbox/app.dart';
 
-void main() => runApp(const CaosApp());
+void main() => runApp(const CaosRoot());
+
+class CaosRoot extends StatelessWidget {
+  const CaosRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CaosBox • beta',
+      theme: ThemeData(useMaterial3: true),
+      home: const CaosBox(),
+    );
+  }
+}
