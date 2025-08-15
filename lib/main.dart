50e484da504d8dc2dea4ecfd27767c71e68f9bb5
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-15 11:19:44 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:caosbox/app.dart';
 
-void main() => runApp(const CaosApp());
+void main() {
+  FlutterError.onError = (details) {
+    FlutterError.dumpErrorToConsole(details);
+  };
+  runZonedGuarded(() {
+    runApp(const CaosApp());
+  }, (e, st) {
+    // En Web verás el error en DevTools > Console
+    // ignore: avoid_print
+    print('Zoned error: $e\n$st');
+  });
+}
