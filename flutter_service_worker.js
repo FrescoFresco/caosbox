21467b60f62dee8608abaf0d3a54a022ef0f6f58
FrescoFresco <FrescoFresco@users.noreply.github.com>
2025-08-15 10:14:07 +0000

    deploy: c1859a5af6b01d3babd0336e6e6e642929d5ac10

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4b2a4d5970a902b240395a416ee0a30e",
+const RESOURCES = {"flutter_bootstrap.js": "75f1cca4e188b65717da4563cc4af1ae",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "version.json": "680f2abb6a468fd89c031151bcc6aa38",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
