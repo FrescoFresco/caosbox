c4378521d38a5747dc159e2774fc634ac1182902
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-15 10:41:48 +0100

    Delete lib directory

--- lib/core/models/filters.dart
+++
@@ -1,3 +0,0 @@
-// Filtros básicos (comunes a todo el proyecto)
-enum FilterMode { off, include, exclude }
-enum FilterKey { completed, archived, hasLinks }
--- lib/data/repositories/link_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-abstract class LinkRepository extends ChangeNotifier {
-  Set<String> linksOf(String id);
-  void toggle(String a, String b);
-  void replaceAll(Map<String, Set<String>> links);
-}
