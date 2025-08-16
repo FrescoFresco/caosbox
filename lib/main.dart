31e2a5ca6795961f3f908866ade84517759441cd
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-16 22:35:20 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Inicializa Firebase con los --dart-define que inyecta tu workflow
   await Firebase.initializeApp(options: firebaseWebOptions());
   runApp(const CaosApp());
 }
