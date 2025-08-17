8a4891848a35efe482c70067aef33418d65c97f4
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-17 03:15:50 +0100

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,58 +1,31 @@
+// lib/app.dart
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:caosbox/app/state/app_state.dart';
+import 'package:caosbox/core/models/enums.dart';
 import 'package:caosbox/data/fire_repo.dart';
-
-import 'package:caosbox/config/blocks.dart';
-import 'package:caosbox/ui/screens/generic_screen.dart';
-import 'package:caosbox/ui/screens/links_block.dart';
+import 'package:caosbox/ui/screens/home_screen.dart';
 
 class CaosApp extends StatefulWidget {
-  const CaosApp({super.key});
-  @override State<CaosApp> createState() => _CaosAppState();
+  final String uid;
+  const CaosApp({super.key, required this.uid});
+
+  @override
+  State<CaosApp> createState() => _CaosAppState();
 }
 
 class _CaosAppState extends State<CaosApp> {
-  late final AppState st;
-
   @override
   void initState() {
     super.initState();
-    st = AppState();
-    final uid = FirebaseAuth.instance.currentUser!.uid;
-    st.attachRepo(FireRepo(FirebaseFirestore.instance, uid));
+    final st = context.read<AppState>();
+    st.attachRepo(FireRepo(FirebaseFirestore.instance, widget.uid), widget.uid);
   }
 
   @override
-  void dispose() { st.dispose(); super.dispose(); }
-
-  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: st,
-      builder: (_, __) => DefaultTabController(
-        length: blocks.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('CaosBox • beta'),
-            bottom: TabBar(
-              tabs: [for (final b in blocks) Tab(icon: Icon(b.icon), text: b.label)],
-            ),
-          ),
-          body: SafeArea(
-            child: TabBarView(
-              children: [
-                for (final b in blocks)
-                  b.type != null
-                    ? GenericScreen(block: b, st: st)
-                    : LinksBlock(st: st),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return const HomeScreen();
   }
 }
