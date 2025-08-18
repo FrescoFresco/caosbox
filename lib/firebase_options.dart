59b11b493c9f5e54a3ea8fe2461f594e5e7cbe2a
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-18 20:58:38 +0100

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,8 +1,6 @@
 // lib/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart';
 
-/// Lee TODAS las claves desde --dart-define en el build de GitHub Actions.
-/// Ojo: las claves aquí SON literales (no variables), por eso compila en const.
 class DefaultFirebaseOptions {
   static const FirebaseOptions currentPlatform = FirebaseOptions(
     apiKey: String.fromEnvironment('FB_API_KEY'),
@@ -11,7 +9,6 @@ class DefaultFirebaseOptions {
     messagingSenderId: String.fromEnvironment('FB_MESSAGING_SENDER_ID'),
     authDomain: String.fromEnvironment('FB_AUTH_DOMAIN'),
     storageBucket: String.fromEnvironment('FB_STORAGE_BUCKET'),
-    // measurementId es opcional en web, pero si la defines mejor:
     measurementId: String.fromEnvironment('FB_MEASUREMENT_ID'),
   );
 }
