898cf3bcce9c42a1a4f12723597dd133e55c7c88
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-18 22:12:36 +0100

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+// lib/firebase_options.dart
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => const FirebaseOptions(
+    apiKey: 'AIzaSyBbpkoc4YlqfuYyM2TYASidFMOpeN9v2e4',
+    appId: '1:1087718443702:web:53c05e5ca672de14b5f417',
+    messagingSenderId: '1087718443702',
+    projectId: 'caosbox-ef75b',
+    authDomain: 'caosbox-ef75b.firebaseapp.com',
+    storageBucket: 'caosbox-ef75b.firebasestorage.app',
+    measurementId: 'G-8C1RD6K5Q5',
+  );
+}
