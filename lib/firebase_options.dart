05978b4100f339e0dcd8527418e1cb9a2deb6ff2
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-19 03:09:34 +0100

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,14 +1,36 @@
 // lib/firebase_options.dart
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+
+/// Lee valores de --dart-define en build time.
+String _env(String k) => const String.fromEnvironment(k, defaultValue: '');
+
+class OptionsFromEnv {
+  final FirebaseOptions web = FirebaseOptions(
+    apiKey: _env('FB_API_KEY'),
+    appId: _env('FB_APP_ID'),
+    messagingSenderId: _env('FB_MESSAGING_SENDER_ID'),
+    projectId: _env('FB_PROJECT_ID'),
+    authDomain: _env('FB_AUTH_DOMAIN'),
+    storageBucket: _env('FB_STORAGE_BUCKET'),
+    measurementId: _env('FB_MEASUREMENT_ID'),
+  );
+}
 
 class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform => const FirebaseOptions(
-    apiKey: 'AIzaSyBbpkoc4YlqfuYyM2TYASidFMOpeN9v2e4',
-    appId: '1:1087718443702:web:53c05e5ca672de14b5f417',
-    messagingSenderId: '1087718443702',
-    projectId: 'caosbox-ef75b',
-    authDomain: 'caosbox-ef75b.firebaseapp.com',
-    storageBucket: 'caosbox-ef75b.firebasestorage.app',
-    measurementId: 'G-8C1RD6K5Q5',
-  );
+  static FirebaseOptions get currentPlatform {
+    final o = OptionsFromEnv();
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.iOS:
+      case TargetPlatform.macOS:
+      case TargetPlatform.android:
+      case TargetPlatform.fuchsia:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        // Si quisieras soportar móviles/escritorio, añade aquí los Options específicos.
+        return o.web;
+      case TargetPlatform.values:
+        return o.web;
+    }
+  }
 }
