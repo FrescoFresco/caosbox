b7782aa9a57d962c314fc78a20e232826ca939ba
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-20 17:55:35 +0100

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => throw UnimplementedError(
+      'Reemplaza firebase_options.dart con el generado por FlutterFire');
+}
