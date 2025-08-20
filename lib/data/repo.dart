06b49a28df9d68294b0169a05e6a1219666727af
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-20 19:25:30 +0100

    Update repo.dart

--- lib/data/repo.dart
+++ lib/data/repo.dart
@@ -1,13 +1,15 @@
 import '../models/item.dart';
 
 abstract class Repo {
+  // Items
   Stream<List<Item>> streamByType(ItemType t);
   Stream<Item?> streamItem(String id);
-  Future<String> addItem(ItemType t, {required String text, String note = '', List<String> tags = const []});
+  Future<String> addItem(ItemType t, {required String text, String note = ''});
   Future<void> updateItem(Item it);
   Future<void> setStatus(String id, ItemStatus s);
   Future<void> deleteItem(String id);
 
+  // Links
   Stream<Set<String>> streamLinksOf(String id);
   Future<void> link(String a, String b);
   Future<void> unlink(String a, String b);
