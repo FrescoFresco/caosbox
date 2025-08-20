a2b8132eef2355052b6189ef6844570e302b5bc1
FrescoFresco <116513345+FrescoFresco@users.noreply.github.com>
2025-08-20 19:11:05 +0100

    Update repo.dart

--- lib/data/repo.dart
+++ lib/data/repo.dart
@@ -1,7 +1,6 @@
 import '../models/item.dart';
 
 abstract class Repo {
-  // Items
   Stream<List<Item>> streamByType(ItemType t);
   Stream<Item?> streamItem(String id);
   Future<String> addItem(ItemType t, {required String text, String note = '', List<String> tags = const []});
@@ -9,14 +8,10 @@ abstract class Repo {
   Future<void> setStatus(String id, ItemStatus s);
   Future<void> deleteItem(String id);
 
-  // Links
   Stream<Set<String>> streamLinksOf(String id);
   Future<void> link(String a, String b);
   Future<void> unlink(String a, String b);
   Future<bool> hasLink(String a, String b);
 }
 
-String canonicalKey(String a, String b) {
-  if (a.compareTo(b) <= 0) return '${a}_$b';
-  return '${b}_$a';
-}
+String canonicalKey(String a, String b) => (a.compareTo(b) <= 0) ? '${a}_$b' : '${b}_$a';
